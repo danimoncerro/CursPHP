6de41b4a455495cd6bddc3d19ac39cda3134179b
root <root@Satellite.(none)>
2017-02-12 10:01:44 +0200

    Creare clasa Detabase

--- Curs31/classes/Database.php
+++ Curs31/classes/Database.php
@@ -2,7 +2,35 @@
 
 class Database {
 
+	public $server='localhost';
+	public $username='root';
+	public $password='';
+	public $database='magazin';
+	public $conn; // Conexiune la baza de date
 
+	// Ne conectam la baza de date 
+	public function __construct() {
+		try {
+			$this->conn = new PDO("mysql:host=$this->server;dbname=$this->database", 
+								$this->username, 
+								$this->password);
+		}
+		catch (PDOException $e) {
+			echo "Conexiune esuata: " . $e->getMessage();
+		}
+	}
 
-	
+	// Verificam conexiunea la baza de date 
+	public function checkConnection() {
+		if ($this->conn) {
+			echo "Conexiune reusita";
+		} else {
+			echo "Conexiune esuata";
+		}
+	}
+
+	// Returneaza conexiunea la baza de date 
+	public function getConnection() {
+		return $this->conn;
+	}
 }
