b7287aff20028d5bd968374c03360e1e63ca2ee7
root <root@Satellite.(none)>
2017-02-13 23:04:03 +0200

    Implementare clasa Cart.

--- Curs31/add_to_cart.php
+++ Curs31/add_to_cart.php
@@ -1,16 +1,15 @@
 <?php 
+  
+  session_start();
+  include 'classes/Cart.php';
+  
+  $cart = new Cart();
 
-  include 'connect.php';
-  include 'functions.php';
-
-  $id = $_GET['id'];
-  $cantitate = $_GET["cantitate"];
-
-  add_to_cart($id, $cantitate);
-
-$_SESSION['message'] = "Am adaugat produsul in cosul de cumparaturi";
-
-
+  $cart->setId($id);
+  $cart->setCantitate($cantitate);
+  $cart->add();
+  
+  $_SESSION['message'] = "Am adaugat produsul in cosul de cumparaturi";
 
   header("Location: view_cart.php");
   exit;
--- Curs31/classes/Cart.php
+++ Curs31/classes/Cart.php
@@ -1,12 +1,60 @@
 <?php 
 
+require_once 'Database.php';
 
 class Cart {
 
+	const TABLENAME = "products"; 
+	protected $id = 0;
+	protected $tip = '';
+	protected $soi = '';
+	protected $culoare = '';
+	protected $pret = 0;
+	protected $cantitate = 0;
+	protected $conn = false ;
+
+	public function __construct() {
+		$db = new Database();
+		$this->conn = $db->getConnection();
+	}
+
+	protected function getConnection() {
+    	return $this->conn;
+    }
+
+    public function setId($_id) {
+    	$this->id = $_id;
+    }
+
+    public function setCantitate ($_cantitate) {
+    	$this->cantitate = $_cantitate;
+    }
 
 
 	public function add() {
 
+		$sql = "SELECT products.*, culori.culoarea
+			FROM products
+			LEFT JOIN culori on products.culoare=culori.id
+			WHERE products.id = $this->id";
+
+		$stmt = $this->conn->prepare($sql);
+
+		$stmt->execute();
+
+		$result = $stmt->fetchAll();
+
+		$product  = $result[0]; // primul element - este singurul element pt ca am cautat dupa id unic.
+
+		// 2. Adauga in cos 
+		$_SESSION['cart'][$this->id] = [
+			"id" => $this->id,
+			"title" => $product["tip"],
+			"soi"	 => $product["soi"],
+			"culoare" =>  $product["culoarea"],
+			"pret" => $product["pret"],
+			"cantitate" => $this->cantitate,
+		];
 	}
 
 
